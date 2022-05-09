a6ffe3a30a859fc68498360ff00be46ab50a883d
mazils <arturasmaziliauskas2@gmail.com>
2022-05-09 11:34:18 +0200

    get user,get company,update company,update user

--- StudyIt/MongoDB/Services/UserService.cs
+++ StudyIt/MongoDB/Services/UserService.cs
@@ -22,8 +22,18 @@ public class UserService
     public async Task CreateUser(User user) =>
         await _userCollection.InsertOneAsync(user);
 
-    // Finding a User
-    public async Task<User?> GetUser(string email) =>
+    // Finding a User by email
+    public async Task<User?> GetUserByEmail(string email) =>
         await _userCollection.AsQueryable<User>()
             .Where(e => e.email == email).FirstOrDefaultAsync();
+
+    // Finding a User by id  
+    public async Task<User?> GetUserbyId(string _id) =>
+        await _userCollection.AsQueryable<User>()
+            .Where(e => e._id == _id).FirstOrDefaultAsync();
+//updating user
+     public async Task<ReplaceOneResult> updateUser(User updatedUser) {
+           var result =  await _userCollection.ReplaceOneAsync(r => r._id == updatedUser._id, updatedUser);
+           return result;
+     }
 }
