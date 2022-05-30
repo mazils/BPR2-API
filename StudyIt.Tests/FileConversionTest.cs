bd2ee88b499b24a56cce53d644bebd2af2e2cf16
mazils <arturasmaziliauskas2@gmail.com>
2022-05-30 22:23:20 +0200

    Create FileConversionTest.cs

---
+++ StudyIt.Tests/FileConversionTest.cs
@@ -0,0 +1,58 @@
+using NUnit.Framework;
+using System;
+namespace StudyIt.Tests;
+
+
+
+public class FileConversionTests
+{
+    // Arrange
+    string base64JpgResult;
+    string base64Jpg;
+    string base64String;
+    string base64StringPng;
+    byte[] byteBase64Result;
+    byte[] binaryBase64;
+    DateTime convertedDateSlash; 
+    string base64ToBinAndBack;
+
+    [SetUp]
+    public void Setup()
+    {
+        // Act
+        base64Jpg = "data:image/jpg;base64,/9j/4AAQSkZJRgABAQEAYABgAAD/2wBDAAIBAQIBAQICAgICAgICAwUDAwMDAwYEBAMFBwYHBwcGBwcICQsJCAgKCAcHCg0KCgsMDAwMBwkODw0MDgsMDAz/2wBDAQICAgMDAwYDAwYMCAcIDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAz/wAARCAAJAA0DASIAAhEBAxEB/8QAHwAAAQUBAQEBAQEAAAAAAAAAAAECAwQFBgcICQoL/8QAtRAAAgEDAwIEAwUFBAQAAAF9AQIDAAQRBRIhMUEGE1FhByJxFDKBkaEII0KxwRVS0fAkM2JyggkKFhcYGRolJicoKSo0NTY3ODk6Q0RFRkdISUpTVFVWV1hZWmNkZWZnaGlqc3R1dnd4eXqDhIWGh4iJipKTlJWWl5iZmqKjpKWmp6ipqrKztLW2t7i5usLDxMXGx8jJytLT1NXW19jZ2uHi4+Tl5ufo6erx8vP09fb3+Pn6/8QAHwEAAwEBAQEBAQEBAQAAAAAAAAECAwQFBgcICQoL/8QAtREAAgECBAQDBAcFBAQAAQJ3AAECAxEEBSExBhJBUQdhcRMiMoEIFEKRobHBCSMzUvAVYnLRChYkNOEl8RcYGRomJygpKjU2Nzg5OkNERUZHSElKU1RVVldYWVpjZGVmZ2hpanN0dXZ3eHl6goOEhYaHiImKkpOUlZaXmJmaoqOkpaanqKmqsrO0tba3uLm6wsPExcbHyMnK0tPU1dbX2Nna4uPk5ebn6Onq8vP09fb3+Pn6/9oADAMBAAIRAxEAPwDp7zX1sVw1vO8uQCsa7gD9ap6n4wNjMFS0EmRk7pdhH4YNfvDRX8zr6OdJb47/AMpP/wCWn9SL6SkU9ct/8rf/AHI//9k=";
+        base64String = "/9j/4AAQSkZJRgABAQEAYABgAAD/2wBDAAIBAQIBAQICAgICAgICAwUDAwMDAwYEBAMFBwYHBwcGBwcICQsJCAgKCAcHCg0KCgsMDAwMBwkODw0MDgsMDAz/2wBDAQICAgMDAwYDAwYMCAcIDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAz/wAARCAAJAA0DASIAAhEBAxEB/8QAHwAAAQUBAQEBAQEAAAAAAAAAAAECAwQFBgcICQoL/8QAtRAAAgEDAwIEAwUFBAQAAAF9AQIDAAQRBRIhMUEGE1FhByJxFDKBkaEII0KxwRVS0fAkM2JyggkKFhcYGRolJicoKSo0NTY3ODk6Q0RFRkdISUpTVFVWV1hZWmNkZWZnaGlqc3R1dnd4eXqDhIWGh4iJipKTlJWWl5iZmqKjpKWmp6ipqrKztLW2t7i5usLDxMXGx8jJytLT1NXW19jZ2uHi4+Tl5ufo6erx8vP09fb3+Pn6/8QAHwEAAwEBAQEBAQEBAQAAAAAAAAECAwQFBgcICQoL/8QAtREAAgECBAQDBAcFBAQAAQJ3AAECAxEEBSExBhJBUQdhcRMiMoEIFEKRobHBCSMzUvAVYnLRChYkNOEl8RcYGRomJygpKjU2Nzg5OkNERUZHSElKU1RVVldYWVpjZGVmZ2hpanN0dXZ3eHl6goOEhYaHiImKkpOUlZaXmJmaoqOkpaanqKmqsrO0tba3uLm6wsPExcbHyMnK0tPU1dbX2Nna4uPk5ebn6Onq8vP09fb3+Pn6/9oADAMBAAIRAxEAPwDp7zX1sVw1vO8uQCsa7gD9ap6n4wNjMFS0EmRk7pdhH4YNfvDRX8zr6OdJb47/AMpP/wCWn9SL6SkU9ct/8rf/AHI//9k=";
+        base64StringPng = "data:image/png;base64,/9j/4AAQSkZJRgABAQEAYABgAAD/2wBDAAIBAQIBAQICAgICAgICAwUDAwMDAwYEBAMFBwYHBwcGBwcICQsJCAgKCAcHCg0KCgsMDAwMBwkODw0MDgsMDAz/2wBDAQICAgMDAwYDAwYMCAcIDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAz/wAARCAAJAA0DASIAAhEBAxEB/8QAHwAAAQUBAQEBAQEAAAAAAAAAAAECAwQFBgcICQoL/8QAtRAAAgEDAwIEAwUFBAQAAAF9AQIDAAQRBRIhMUEGE1FhByJxFDKBkaEII0KxwRVS0fAkM2JyggkKFhcYGRolJicoKSo0NTY3ODk6Q0RFRkdISUpTVFVWV1hZWmNkZWZnaGlqc3R1dnd4eXqDhIWGh4iJipKTlJWWl5iZmqKjpKWmp6ipqrKztLW2t7i5usLDxMXGx8jJytLT1NXW19jZ2uHi4+Tl5ufo6erx8vP09fb3+Pn6/8QAHwEAAwEBAQEBAQEBAQAAAAAAAAECAwQFBgcICQoL/8QAtREAAgECBAQDBAcFBAQAAQJ3AAECAxEEBSExBhJBUQdhcRMiMoEIFEKRobHBCSMzUvAVYnLRChYkNOEl8RcYGRomJygpKjU2Nzg5OkNERUZHSElKU1RVVldYWVpjZGVmZ2hpanN0dXZ3eHl6goOEhYaHiImKkpOUlZaXmJmaoqOkpaanqKmqsrO0tba3uLm6wsPExcbHyMnK0tPU1dbX2Nna4uPk5ebn6Onq8vP09fb3+Pn6/9oADAMBAAIRAxEAPwDp7zX1sVw1vO8uQCsa7gD9ap6n4wNjMFS0EmRk7pdhH4YNfvDRX8zr6OdJb47/AMpP/wCWn9SL6SkU9ct/8rf/AHI//9k=";
+        base64JpgResult = FileConversion.AddFileExtension(base64String);
+        byteBase64Result = FileConversion.Base64StringtoBin(base64String);
+        convertedDateSlash = FileConversion.DateStringToDate("2018/7/24");
+        base64ToBinAndBack = FileConversion.BinToBase64String(FileConversion.Base64StringtoBin(base64String));
+
+    }
+    //Assert
+    [Test]
+    public void base64EqualPass()
+    {
+        Assert.AreEqual(base64JpgResult, base64Jpg);
+    }
+    [Test]
+    public void Base64EqualFails()
+    {
+        Assert.AreNotEqual(base64JpgResult, base64StringPng);
+    }
+    [Test]
+    public void Base64ToBinAndBackPass()
+    {
+        Assert.AreEqual(base64String, base64ToBinAndBack);
+    }
+    [Test]
+    public void DateStringToDatePassDash()
+    {
+        Assert.AreEqual(new DateTime(2018, 7, 24), convertedDateSlash);
+    }
+    [Test]
+    public void DateStringToDatePassSlash()
+    {
+        Assert.AreEqual(new DateTime(2018, 7, 24), convertedDateSlash);
+    }
+}
